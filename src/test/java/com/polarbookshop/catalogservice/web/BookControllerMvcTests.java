f9505d873ae79a178efaf75cb7bb3e3d25e50eb0
jempitman <80392341+jempitman@users.noreply.github.com>
2023-02-02 16:09:04 +0100

    refactor: update tests for persistent Book objects

--- src/test/java/com/polarbookshop/catalogservice/web/BookControllerMvcTests.java
+++ src/test/java/com/polarbookshop/catalogservice/web/BookControllerMvcTests.java
@@ -1,6 +1,7 @@
-package com.polarbookshop.catalogservice.domain;
+package com.polarbookshop.catalogservice.web;
 
-import com.polarbookshop.catalogservice.web.BookController;
+import com.polarbookshop.catalogservice.domain.BookNotFoundException;
+import com.polarbookshop.catalogservice.domain.BookService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -8,8 +9,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BookController.class)
 public class BookControllerMvcTests {
