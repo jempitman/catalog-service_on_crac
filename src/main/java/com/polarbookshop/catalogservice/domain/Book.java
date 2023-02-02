3449527bbec60decb282867d6359f60ba5d0491b
jempitman <80392341+jempitman@users.noreply.github.com>
2023-02-02 16:08:17 +0100

    feat: persist Book objects

--- src/main/java/com/polarbookshop/catalogservice/domain/Book.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/Book.java
@@ -1,11 +1,17 @@
 package com.polarbookshop.catalogservice.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Positive;
 
 public record Book(
+
+        @Id
+        Long id,
 
         @NotBlank(message = "The book ISBN must be defined.")
         @Pattern(
@@ -28,5 +34,16 @@ public record Book(
         @Positive(
                 message = "The book price must be greater than zero."
         )
-        Double price
-) {}
+        Double price,
+
+        @Version
+        int version
+) {
+        public static Book of (
+                String isbn, String title, String author, Double price
+        ) {
+                return new Book(
+                        null, isbn, title, author, price, 0
+                );
+        }
+}
