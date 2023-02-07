41630104f2ee0e8df4b1240c26040897b307fd5a
jempitman <80392341+jempitman@users.noreply.github.com>
2023-02-07 14:24:13 +0100

    feat: evolve db with optional publisher column

--- src/main/java/com/polarbookshop/catalogservice/domain/Book.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/Book.java
@@ -39,6 +39,8 @@ public record Book(
         )
         Double price,
 
+        String publisher,
+
         @CreatedDate
         Instant createdDate,
 
@@ -49,10 +51,10 @@ public record Book(
         int version
 ) {
         public static Book of (
-                String isbn, String title, String author, Double price
+                String isbn, String title, String author, Double price, String publisher
         ) {
                 return new Book(
-                        null, isbn, title, author, price, null, null, 0
-                );
+                        null, isbn, title, author, price, publisher,null,
+                        null, 0);
         }
 }
