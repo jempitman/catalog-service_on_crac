1e1c6c3150a0917386fd38dac15422f35c24e616
jempitman <jempitman@gmail.com>
2023-03-07 16:48:23 +0100

    test: add keycloak authorization for integration tests

--- src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -1,17 +1,73 @@
 package com.polarbookshop.catalogservice;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.polarbookshop.catalogservice.domain.Book;
+import dasniko.testcontainers.keycloak.KeycloakContainer;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.http.MediaType;
+
+import org.springframework.http.HttpHeaders;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("integration")
+@Testcontainers
 class CatalogServiceApplicationTests {
+
+	public static KeycloakToken bjornTokens;
+	public static KeycloakToken isabelleTokens;
+
+	@BeforeAll
+	static void generateAccessTokens(){
+		WebClient webClient = WebClient.builder()
+				.baseUrl(keyCloakContainer.getAuthServerUrl()
+				+ "realms/PolarBookshop/protocol/openid-connect/token")
+				.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+				.build();
+
+		isabelleTokens = authenticateWith(
+				"isabelle", "password", webClient);
+		bjornTokens = authenticateWith(
+				"bjorn", "password", webClient);
+	}
+
+	private static KeycloakToken authenticateWith(
+			String username, String password, WebClient webClient
+	) {
+		return webClient
+				.post()
+				.body(
+						BodyInserters.fromFormData("grant_type", "password")
+								.with("client_id", "polar-test")
+								.with("username", username)
+								.with("password", password)
+				)
+				.retrieve()
+				.bodyToMono(KeycloakToken.class)
+				.block();
+	}
+
+	private record KeycloakToken(String accessToken){
+		@JsonCreator
+		private KeycloakToken(
+				@JsonProperty("access_token") final String accessToken
+		) {
+			this.accessToken = accessToken;
+		}
+	}
 
 	@Test
 	void contextLoads() {
@@ -19,6 +75,17 @@ class CatalogServiceApplicationTests {
 
 	@Autowired
 	private WebTestClient webTestClient;
+
+	@Container
+	private static final KeycloakContainer keyCloakContainer =
+			new KeycloakContainer("quay.io/keycloak/keycloak:19.0")
+				.withRealmImportFile("test-realm-config.json");
+
+	@DynamicPropertySource
+	static void dynamicProperties(DynamicPropertyRegistry registry){
+		registry.add("spring.security.oauth2.resourceserver.jwt.issuer-uri",
+				() -> keyCloakContainer.getAuthServerUrl() + "realms/PolarBookshop");
+	}
 
 	@Test
 	void whenGetRequestWithIdThenBookReturned() {
@@ -51,6 +118,8 @@ class CatalogServiceApplicationTests {
 		webTestClient
 				.post()
 				.uri("/books")
+				.headers(headers ->
+						headers.setBearerAuth(isabelleTokens.accessToken()))
 				.bodyValue(expectedBook)
 				.exchange()
 				.expectStatus().isCreated()
