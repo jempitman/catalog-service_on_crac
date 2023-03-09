010f9ab67cf7f3bdbf61429421371c71f475740b
jempitman <jempitman@gmail.com>
2023-03-09 15:50:45 +0100

    test: find all books

--- src/test/java/com/polarbookshop/catalogservice/domain/BookRepositoryJdbcTests.java
+++ src/test/java/com/polarbookshop/catalogservice/domain/BookRepositoryJdbcTests.java
@@ -10,15 +10,14 @@ import org.springframework.data.jdbc.core.JdbcAggregateTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJdbcTest
 @Import(DataConfig.class)
-@AutoConfigureTestDatabase(
-
-        replace = AutoConfigureTestDatabase.Replace.NONE
-)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("integration")
 public class BookRepositoryJdbcTests {
 
@@ -38,4 +37,21 @@ public class BookRepositoryJdbcTests {
         assertThat(actualBook).isPresent();
         assertThat(actualBook.get().isbn()).isEqualTo(book.isbn());
     }
+
+    @Test
+    void findAllBooks(){
+        var book1 = Book.of("1234561235", "Title", "Author", 12.90, "Polarsophia");
+        var book2 = Book.of("1234561236", "Another Title", "Author", 12.90, "Polarsophia");
+
+        jdbcAggregateTemplate.insert(book1);
+        jdbcAggregateTemplate.insert(book2);
+
+        Iterable<Book> actualBooks = bookRepository.findAll();
+
+        assertThat(StreamSupport.stream(actualBooks.spliterator(), true)
+                .filter(book -> book.isbn().equals(book1.isbn()) || book.isbn().equals(book2.isbn()))
+                .collect(Collectors.toList())).hasSize(2);
+    }
+
+
 }
